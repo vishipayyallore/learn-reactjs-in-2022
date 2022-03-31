61a47a8d82f4b531597139b429854dbbecd4455d
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-31 13:28:19 +0530

    Header Part 1

--- a1-eshop/src/App.js
+++ a1-eshop/src/App.js
@@ -1,41 +1,12 @@
 import './App.css';
+import Header from './Header';
 
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
     <div className="App">
 
-      <div className="header__logo">
-        <StorefrontIcon fontSize="large" className="header__logoImage" />
-        <h2 className="header__logoImage">eShop</h2>
-      </div>
-
-      <div className="header__search">
-        <input type="text" placeholder="Search" className="header__searchInput" />
-        <SearchIcon className="header__searchIcon" />
-      </div>
-
-      <div className="header__nav">
-
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
-        </div>
-
-        <div className="nav__item">
-          <ShoppingBasketIcon />
-          <span className="nav__itemLineTwo">9</span>
-        </div>
-
-      </div>
+      <Header></Header>
 
     </div>
   );
---
+++ a1-eshop/src/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Header.css";
+
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <StorefrontIcon fontSize="large" className="header__logoImage" />
+                <h2 className="header__logoImage">eShop</h2>
+            </div>
+
+            <div className="header__search">
+                <input type="text" placeholder="Search" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header__nav">
+
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Hello Guest</span>
+                    <span className="nav__itemLineTwo">Sign In</span>
+                </div>
+
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Your</span>
+                    <span className="nav__itemLineTwo">Shop</span>
+                </div>
+
+                <div className="nav__item">
+                    <ShoppingBasketIcon className="itemBasket" />
+                    <span className="nav__itemLineTwo nav__basketCount">9</span>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Header;
